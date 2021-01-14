2c4e12896e35e6d2d8dec3cbbfe90e566e522e06
kkimlee <40078267+kkimlee@users.noreply.github.com>
2021-01-14 19:57:29 +0900

    Update FieldElement.py

--- Chapter01 유한체/FieldElement.py
+++ Chapter01 유한체/FieldElement.py
@@ -41,5 +41,6 @@ class FieldElement:
         return self.__class__(num, self.prime)
     
     def __pow__(self, exponent):
-        num = self.num ** exponent) % self.prime
+        n = exponent % (self.exponent - 1)
+        num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
