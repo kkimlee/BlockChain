843769517b4d5d2c811b507cf6d817c2c12825b1
kkimlee <40078267+kkimlee@users.noreply.github.com>
2021-01-14 20:04:00 +0900

    Update FieldElement.py

--- Chapter01 유한체/FieldElement.py
+++ Chapter01 유한체/FieldElement.py
@@ -44,3 +44,14 @@ class FieldElement:
         n = exponent % (self.exponent - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+    if self.prime != other.prime:
+        raise TypeError('Cannot divide two numbers to different Fields')
+    # use Fermat's little theorem:
+    # self.num**(p-1) % p == 1
+    # this means:
+    # 1/n == pow(n, p-2, p)
+    # we return an element of the same class
+    num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+    return self.__class__(num, self.prime)
