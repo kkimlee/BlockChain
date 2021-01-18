5bae1580af756c5986c634d81667c3da2c8b5907
kkimlee <40078267+kkimlee@users.noreply.github.com>
2021-01-18 19:25:09 +0900

    chapter02

---
+++ Chapter02 타원곡선/FieldElement.py
@@ -0,0 +1,57 @@
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+    
+    def __ne__(self, other):
+        if other is None:
+            print('other is none')
+            return False
+        return not(self == other)
+    
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+    
+    def __sub__(self, other):
+        if self.prime != other.prime: 
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num - other.num) % self.prime 
+        return self.__class__(num, self.prime)
+    
+    def __mul__(self, other):
+        if self.prime != other.prime: 
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num * other.num) % self.prime 
+        return self.__class__(num, self.prime)
+    
+    def __pow__(self, exponent):
+        n = exponent % (self.exponent - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers to different Fields')
+        # use Fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        # we return an element of the same class
+        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+        return self.__class__(num, self.prime)
