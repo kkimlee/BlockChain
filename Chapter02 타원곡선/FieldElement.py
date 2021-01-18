e21461dbd9ae8a56153f46f00cc857465b8c6892
kkimlee <40078267+kkimlee@users.noreply.github.com>
2021-01-18 20:41:45 +0900

    Update FieldElement.py

--- Chapter02 타원곡선/FieldElement.py
+++ Chapter02 타원곡선/FieldElement.py
@@ -1,57 +1,24 @@
-class FieldElement:
+class Point:
 
-    def __init__(self, num, prime):
-        if num >= prime or num < 0:
-            error = 'Num {} not in field range 0 to {}'.format(
-                num, prime - 1)
-            raise ValueError(error)
-        self.num = num
-        self.prime = prime
-
-    def __repr__(self):
-        return 'FieldElement_{}({})'.format(self.prime, self.num)
-
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+            
     def __eq__(self, other):
-        if other is None:
-            return False
-        return self.num == other.num and self.prime == other.prime
-    
-    def __ne__(self, other):
-        if other is None:
-            print('other is none')
-            return False
-        return not(self == other)
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
     
     def __add__(self, other):
-        if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num + other.num) % self.prime
-        return self.__class__(num, self.prime)
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
     
-    def __sub__(self, other):
-        if self.prime != other.prime: 
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num - other.num) % self.prime 
-        return self.__class__(num, self.prime)
-    
-    def __mul__(self, other):
-        if self.prime != other.prime: 
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num * other.num) % self.prime 
-        return self.__class__(num, self.prime)
-    
-    def __pow__(self, exponent):
-        n = exponent % (self.exponent - 1)
-        num = pow(self.num, n, self.prime)
-        return self.__class__(num, self.prime)
-
-    def __truediv__(self, other):
-        if self.prime != other.prime:
-            raise TypeError('Cannot divide two numbers to different Fields')
-        # use Fermat's little theorem:
-        # self.num**(p-1) % p == 1
-        # this means:
-        # 1/n == pow(n, p-2, p)
-        # we return an element of the same class
-        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
-        return self.__class__(num, self.prime)
